beb457b0f88b4a9ba636b1d24be33cadc43ee13b
squidgyberries <squidge@tuta.io>
2025-05-18 10:38:26 +0800

    test

--- detectron2/modeling/meta_arch/rcnn.py
+++ detectron2/modeling/meta_arch/rcnn.py
@@ -206,7 +206,8 @@ class GeneralizedRCNN(nn.Module):
         if detected_instances is None:
             if self.proposal_generator is not None:
                 proposals, _ = self.proposal_generator(images, features, None)
-                print(proposals.get("proposal_boxes").tensor.cpu().numpy())
+                print(proposals)
+                print(type(proposals))
             else:
                 assert "proposals" in batched_inputs[0]
                 proposals = [x["proposals"].to(self.device) for x in batched_inputs]
